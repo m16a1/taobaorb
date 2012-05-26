d84099a7c0d5112cc624a959852d5440667ccd7d
m16a1 <a741su@gmail.com>
2012-05-26 13:39:05 +0400

    Retrieving product properties by product ID

--- lib/taobao/product.rb
+++ lib/taobao/product.rb
@@ -1,11 +1,50 @@
 class Taobao::Product
-  attr_reader :cid, :num_iid, :title, :nick, :price, :pic_url, :type, :score,
-              :delist_time, :post_fee, :volume
-  alias :id :num_iid 
+  BASIC_PROPERTIES = [:cid, :num_iid, :title, :nick, :price, :pic_url, :type,
+     :score, :delist_time, :post_fee, :volume]
+
+  OTHER_PROPERTIES = [:detail_url, :seller_cids, :props, :input_pids,
+    :input_str, :desc, :num, :valid_thru, :list_time, :stuff_status,
+    :location, :express_fee, :ems_fee, :has_discount, :freight_payer,
+    :has_invoice, :has_warranty, :has_showcase, :modified, :increment,
+    :approve_status, :postage_id, :product_id, :auction_point,
+    :property_alias, :item_img, :prop_img, :sku, :video, :outer_id,
+    :is_virtual]
+  
+  attr_reader *BASIC_PROPERTIES
+  alias :id :num_iid
   
   def initialize(product_properties)
-    product_properties.each do |k, v|
-      self.instance_variable_set("@#{k}", v)
+    @all_properties_fetched = false
+    
+    if Hash === product_properties
+      hash_to_object(product_properties)
+    else
+      @id = product_properties.to_s
+      fetch_full_data
     end
+  end
+  
+  def method_missing(method_name, *args, &block)
+      unless OTHER_PROPERTIES.include? method_name
+        super
+      else
+        fetch_full_data unless @all_properties_fetched
+        self.instance_variable_get "@#{method_name}"
+      end
+  end
+  
+  private
+  def hash_to_object(hash)
+    hash.each do |k, v|
+      self.instance_variable_set "@#{k}", v
+    end
+  end
+  
+  def fetch_full_data
+    fields = (BASIC_PROPERTIES + OTHER_PROPERTIES).join ','
+    params = {method: 'taobao.item.get', fields: fields, num_iid: @id}
+    result = Taobao.api_request(params)
+    hash_to_object(result[:item_get_response][:item])
+    @all_properties_fetched = true
   end
 end
---
+++ spec/rspec/product_spec.rb
@@ -0,0 +1,46 @@
+# coding: utf-8
+require 'spec_helper'
+
+describe Taobao::Product do
+  describe 'initialize with id' do
+    fixture = 'product.json'.json_fixture
+    args = {
+      method: 'taobao.item.get',
+      fields: 'cid,num_iid,title,nick,price,pic_url,type,score,delist_time,' +
+              'post_fee,volume,detail_url,seller_cids,props,input_pids,' +
+              'input_str,desc,num,valid_thru,list_time,stuff_status,' +
+              'location,express_fee,ems_fee,has_discount,freight_payer,' +
+              'has_invoice,has_warranty,has_showcase,modified,increment,' +
+              'approve_status,postage_id,product_id,auction_point,' +
+              'property_alias,item_img,prop_img,sku,video,outer_id,is_virtual',
+      num_iid: '2997802325'
+    }
+    
+    it 'should returns product' do
+      Taobao.stub(:api_request).with(args).and_return(fixture)
+      product = Taobao::Product.new(2997802325)  
+
+      product.cid.should == 1512
+      product.nick.should == '奇迹shouji'
+      product.num_iid.should == 2997802325
+      product.pic_url.should == 'http://img03.taobaocdn.com/bao/uploaded/i3/T1YsfeXbxkXXXpZak._110941.jpg'
+      product.price.should == '4300.00'
+      product.title.should be == '可完美越狱 Apple/苹果 iPhone 4S 正品 装软件 未拆封 未激活'
+
+      product.approve_status.should == 'onsale'
+      product.auction_point.should == 0
+      product.delist_time.should == '2012-06-01 22:41:44'
+      product.type.should == 'fixed'
+    end
+    
+    describe 'get unknown product property' do
+      it 'should raise an exception' do
+        Taobao.stub(:api_request).with(args).and_return(fixture)
+        product = Taobao::Product.new(2997802325)
+        lambda {product.unknown_property}
+          .should raise_error NoMethodError
+      end
+    end
+    
+  end
+end
