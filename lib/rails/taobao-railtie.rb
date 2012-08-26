d48f2b01f03b0f68578443dfbe2da077eea91986
m16a1 <a741su@gmail.com>
2012-08-26 23:20:58 +0400

    Remove debug output

--- lib/rails/taobao-railtie.rb
+++ lib/rails/taobao-railtie.rb
@@ -7,7 +7,6 @@ module Taobao
         config = YAML.load_file(Rails.root.join('config', 'taobaorb.yml'))[Rails.env]
         Taobao.public_key = config['public_key']
         Taobao.private_key = config['private_key']
-        p Taobao.public_key
       rescue Errno::ENOENT
         puts 'Taobao config not found.'
         puts 'To generate one run: rails g taobao:config'
