6b945cd79257d1a75a97986eaa23a02ce18f3b51
m16a1 <a741su@gmail.com>
2012-10-30 20:04:45 +0400

    rename directory

--- spec/lib/user_spec.rb
+++ spec/lib/user_spec.rb
@@ -2,24 +2,51 @@
 require 'spec_helper'
 
 describe Taobao::User do
+  let(:valid_user) do
+    args = {
+      method: 'taobao.user.get',
+      fields: 'user_id,uid,nick,sex,buyer_credit,seller_credit,location,' +
+        'created,last_visit,birthday,type,status,alipay_no,alipay_account,'+
+        'alipay_account,email,consumer_protection,alipay_bind',
+      nick: '喜客多2008'
+    }
+    Taobao.stub(:api_request).with(args).and_return 'user'.json_fixture
+    user = Taobao::User.new('喜客多2008')
+  end
+
+  describe '#nick' do
+    it "return user's nickname" do
+      valid_user.nick.should == '喜客多2008'
+    end
+  end
+
+  describe '#good_purchases_count' do
+    it 'return count of purchases with good rating' do
+      valid_user.good_purchases_count.should == 1292
+    end
+  end
+
+  describe '#buyer_level' do
+    it 'return buyer level' do
+      valid_user.buyer_level.should == 8
+    end
+  end
+
+  describe '#buyer_score' do
+    it 'return buyer score' do
+      valid_user.buyer_score.should == 1292
+    end
+  end
+
+  describe 'total_purchases_count' do
+    it 'should ' do
+      valid_user.total_purchases_count.should == 1292
+    end
+  end
+
+=begin
   describe 'getting user by valid nickname' do
     it 'should returns valid user info' do
-      fixture = 'user'.json_fixture
-      args = {
-        method: 'taobao.user.get',
-        fields: 'user_id,uid,nick,sex,buyer_credit,seller_credit,location,' +
-          'created,last_visit,birthday,type,status,alipay_no,alipay_account,'+
-          'alipay_account,email,consumer_protection,alipay_bind',
-        nick: '喜客多2008'
-      }
-      Taobao.stub(:api_request).with(args).and_return(fixture)
-
-      user = Taobao::User.new('喜客多2008')
-
-      user.nick.should == '喜客多2008'
-      user.good_purchases_count.should == 1292
-      user.buyer_level.should == 8
-      user.buyer_score.should == 1292
       user.total_purchases_count.should == 1292
 
       user.good_sales_count.should == 2587208
@@ -39,7 +66,7 @@ describe Taobao::User do
       user.uid.should == '6cd6014f007c04426e2437fef870329a'
       user.id.should == 18139021
     end
-  end
+=end
   describe 'getting user who does not specify his/her sex' do
     it 'should returns :unknown value' do
       fixture = 'user_unknown_sex'.json_fixture
