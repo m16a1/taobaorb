b7b8281265eee4832a317d270d1cf74b5fa5aaea
m16a1 <a741su@gmail.com>
2013-03-10 14:06:02 +0400

    product spec refactored

--- spec/lib/product_spec.rb
+++ spec/lib/product_spec.rb
@@ -19,31 +19,45 @@ describe Taobao::Product do
     Taobao.stub(:api_request).with(args).and_return(fixture)
     Taobao::Product.new(2997802325)
   end
-  subject { product }
 
   describe 'initialize with id' do
-    it 'should returns product' do
-      subject.cid.should == 1512
-      subject.nick.should == '奇迹shouji'
-      subject.num_iid.should == 2997802325
-      subject.pic_url.should == 'http://img01.taobaocdn.com/bao/uploaded/i1/T1XPLZXhJmXXXmXQ.0_035913.jpg'
-      subject.price.should == 4530.00
-      subject.title.should == 'Apple/苹果 iPhone 4S 未激活送礼包 完美越狱'
-
-      subject.approve_status.should == 'onsale'
-      subject.auction_point.should == 0
-      subject.delist_time.should == DateTime.new(2013,03,11, 12,25,28)
-      subject.type.should == 'fixed'
+    it 'has category ID' do
+      product.cid.should == 1512
+    end
+    it "has seller's nickname" do
+      product.nick.should == '奇迹shouji'
+    end
+    it 'has item id' do
+      product.num_iid.should == 2997802325
+    end
+    it 'has picture url' do
+      product.pic_url.should == 'http://img01.taobaocdn.com/bao/uploaded/i1/T1XPLZXhJmXXXmXQ.0_035913.jpg'
+    end
+    it 'has product price' do
+      product.price.should == 4530.00
+    end
+    it 'has product title' do
+      product.title.should == 'Apple/苹果 iPhone 4S 未激活送礼包 完美越狱'
+    end
+    it 'has approve status' do
+      product.approve_status.should == 'onsale'
+    end
+    it 'has auction point' do
+      product.auction_point.should == 0
+    end
+    it 'has delist time' do
+      product.delist_time.should == DateTime.new(2013,03,11, 12,25,28)
+    end
+    it 'has product type' do
+      product.type.should == 'fixed'
     end
 
-    describe 'get unknown product property' do
-      it do
-        expect {
-          subject.unknown_property
-        }.to raise_error NoMethodError
-      end
+    it 'raises an exception when unknown property retrieved' do
+      expect {
+        product.unknown_property
+      }.to raise_error NoMethodError
     end
   end
-
+  subject { product }
   its(:user) { should be_a_kind_of Taobao::User }
 end
